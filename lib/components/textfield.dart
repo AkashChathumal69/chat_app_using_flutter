ee8fee96f86feabf5d0e018133aa3c05f9d4e666
AkashChathumal69 <akashchathumal470630@gmail.com>
2025-03-09 17:16:46 +0530

    create login page

--- lib/components/textfield.dart
+++ lib/components/textfield.dart
@@ -2,30 +2,31 @@ import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final String hintText;
-  const CustomTextField({super.key, required this.hintText});
+  final bool obscureText;
+
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    required this.obscureText,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: TextField(
+        obscureText: false,
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Theme.of(context).colorScheme.tertiary,
-              width: 2,
-            ),
+            borderSide: BorderSide(color: Colors.white, width: 2),
           ),
           focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Theme.of(context).colorScheme.primary,
-              width: 2,
-            ),
+            borderSide: BorderSide(color: Colors.white, width: 2),
           ),
-          fillColor: Theme.of(context).colorScheme.secondary,
+          fillColor: Colors.grey.shade200,
           filled: true,
           hintText: hintText,
-          hintStyle: TextStyle(color: Theme.of(context).colorScheme.tertiary),
+          hintStyle: TextStyle(color: Colors.grey.shade500),
         ),
       ),
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,33 +7,30 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
+      backgroundColor: Colors.grey.shade300,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             //logo
-            Icon(
-              Icons.message,
-              size: 100,
-              color: Theme.of(context).colorScheme.primary,
-            ),
+            Icon(Icons.message, size: 100, color: Colors.grey.shade500),
             SizedBox(height: 20),
             //welcome text
             Text(
-              'Welcome to ChatMe',
-              style: TextStyle(
-                fontSize: 20,
-                color: Theme.of(context).colorScheme.primary,
-              ),
+              'Welcome to ChatMe!',
+              style: TextStyle(fontSize: 20, color: Colors.grey.shade500),
             ),
+            SizedBox(height: 20),
             //email textfield
-            CustomTextField(hintText: 'Email'),
+            CustomTextField(hintText: 'Email', obscureText: false),
+            SizedBox(height: 10),
+
             SizedBox(height: 10),
             //password textfield
-            CustomTextField(hintText: 'Password'),
+            CustomTextField(hintText: 'Password', obscureText: false),
             SizedBox(height: 10),
             //login button
+
             //register button
           ],
         ),
